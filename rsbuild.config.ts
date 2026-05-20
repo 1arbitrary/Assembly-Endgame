b65656272d0200504bfd12d8ac4b3dde0e7646b1
dot <rawatkushagra33@gmail.com>
2026-05-20 11:09:12 +0530

    re: added rstest and rslint

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,17 +1,27 @@
-import { defineConfig } from "@rsbuild/core";
-import { pluginReact } from "@rsbuild/plugin-react";
+import { defineConfig } from '@rsbuild/core';
+import { pluginBabel } from '@rsbuild/plugin-babel';
+import { pluginReact } from '@rsbuild/plugin-react';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   html: {
     favicon: "./src/favicon.jpg",
-    title: "Assembly: Endgame ",
+    title: "Assembly Endgame",
     meta: {
       charset: {
         charset: "UTF-8",
       },
       viewport: "width=device-width, initial-scale=1.0",
-    },
+    }
   },
-  plugins: [pluginReact()],
+  plugins: [
+    pluginReact(),
+    pluginBabel({
+      include: /\.[jt]sx?$/,
+      exclude: [/[\\/]node_modules[\\/]/],
+      babelLoaderOptions(opts) {
+        opts.plugins?.unshift('babel-plugin-react-compiler');
+      },
+    }),
+  ],
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AssemblyEndGame from "./App";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 const rootEl = document.getElementById("root");
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <AssemblyEndGame />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AssemblyEndGame />} />
+      </Routes>
+      </BrowserRouter>
     </React.StrictMode>,
   );
 }
